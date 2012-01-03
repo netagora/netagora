6be9d01d80ef0f795ab1117672bcf3062dbcfa3a
Sarah Khalil <netagora2012@yahoo.fr>
2012-01-03 15:17:24 +0100

    change routing to delete acme

--- src/ECE/netagoraBundle/Controller/DefaultController.php
+++ src/ECE/netagoraBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/Welcome/{name}")
      * @Template()
      */
     public function indexAction($name)
