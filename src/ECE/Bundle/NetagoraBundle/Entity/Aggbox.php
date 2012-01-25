171a79292a80a13b822091cab8e5f4b12288d231
Sarah Khalil <netagora2012@yahoo.fr>
2012-01-25 02:43:31 +0100

    getter and setter aggbox

--- src/ECE/Bundle/NetagoraBundle/Entity/Aggbox.php
+++ src/ECE/Bundle/NetagoraBundle/Entity/Aggbox.php
@@ -30,4 +30,97 @@ class Aggbox
      * @var datetime $submitDate
      */
     private $submitDate;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+    
+    /**
+     * Get user
+     *
+     * @return User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+    
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    /**
+     * Set content
+     *
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+    
+    /**
+     * Set submitDate
+     *
+     * @param datetime $submitDate
+     */
+    public function setSubmitDate(\DateTime $date)
+    {
+        $this->submitDate = $date;
+    }
+
+    /**
+     * Get submitDate
+     *
+     * @return datetime 
+     */
+    public function getSubmitDate()
+    {
+        return $this->submitDate;
+    }
+    
+    
+    
 }
